27e301ae04408c744dd69e075181b22337425862
Mya Tse <myagirlnc@gmail.com>
2021-07-02 15:04:54 -0400

    Actions and Outlits

---
+++ Actions-&-Outlits/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Actions-&-Outlits
+//
+//  Created by  Scholar on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+        }
+    }
+    
+
+}
+
